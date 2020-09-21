376e03380b5c3a950566af24deabebf89e247596
Nebula16 <tjddns1016@gmail.com>
2020-09-21 23:18:28 +0900

    샘플맵구현, 1인칭 플레이어 구현
    테스트를 위한 샘플맵을 구현
    1인칭 플레이를 위한 기초 코드 구현

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{   
+    // 이동속도
+    public float move_speed;
+    // 점프범위
+    public float jump_range;
+    // 회전속도
+    public float rotate_speed;
+    // 카메라 상하회전 범위
+    public float camera_rotation_limit;
+
+    public Camera my_camera;
+    private float camer_rotation;
+    private Rigidbody rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotation();
+        Camera_Rotation();
+    }
+
+    private void Move()
+    {
+        float X_move = Input.GetAxisRaw("Horizontal");
+        float Z_move = Input.GetAxisRaw("Vertical");
+
+        Vector3 Horizontla_move = transform.right * X_move;
+        Vector3 Vertical_move = transform.forward * Z_move;
+        Vector3 _move = (Horizontla_move + Vertical_move).normalized * move_speed;
+
+        rig.MovePosition(transform.position + _move * Time.deltaTime);
+    }
+
+    private void Rotation()
+    {
+        float _rotate = Input.GetAxisRaw("Mouse X");
+        Vector3 player_rotaion = new Vector3(0.0f, _rotate, 0.0f) * rotate_speed;
+        rig.MoveRotation(rig.rotation * Quaternion.Euler(player_rotaion));
+    }
+
+    private void Camera_Rotation()
+    {
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * rotate_speed;
+        camer_rotation -= _cameraRotationX;
+        camer_rotation = Mathf.Clamp(camer_rotation, -camera_rotation_limit, camera_rotation_limit);
+
+        my_camera.transform.localEulerAngles = new Vector3(camer_rotation, 0f, 0f);
+    }
+
+}
